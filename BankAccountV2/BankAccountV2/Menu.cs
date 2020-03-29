b5ab274b702aa9dc4d286f62807696c84496303d
jacekg93 <jacekg93@gmail.com>
2020-03-29 15:41:21 +0200

    Initial commit - Bank Account v1

---
+++ BankAccountV2/BankAccountV2/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountV2
+{
+    class Menu
+    {
+        public static void MainMenuDisplay()
+        {
+            Console.WriteLine("Bank Account MENU:\n1: Sing in\n2: Create account\n3: Delete account\n4: Exit");
+            Console.Write("\nChoose an option: ");
+        }
+
+        public static void MainMenuOptions(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    CreateAccountMenuDisplay();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        public static void CreateAccountMenuDisplay()
+        {
+            Console.Clear();
+            Console.WriteLine("Create Account");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BankAccountV2/BankAccountV2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MainMenuChoosedOption;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Menu.MainMenuDisplay();
+                MainMenuChoosedOption = int.Parse(Console.ReadLine());
+                if (MainMenuChoosedOption == 4)
+                {
+                    break;
+                }
+
+                Menu.MainMenuOptions(MainMenuChoosedOption);
+            }
+
+            /*
+            try
+            {
+                BankAccount InvalidAccount = new BankAccount("Invalid", -100);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            
+
+            try
+            {
+                myAccount.MakeWithdrawal(40000, DateTime.Now, "For a car");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            */
+            Console.ReadKey();
+        }
+    }
+}
